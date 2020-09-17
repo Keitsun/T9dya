bb4246635d7c4cefbbbe2513b19228fa8d838a0c
Keitsun <ashirogimoto109@gmail.com>
2020-09-17 22:56:18 +0200

    first commit of T9dya

---
+++ script.js
@@ -0,0 +1,12 @@
+function opennav(){
+    document.getElementById("mobile-menu").style.width = "100%"
+}
+
+function closenav(){
+    document.getElementById("mobile-menu").style.width = "0"
+}
+
+
+
+
+
