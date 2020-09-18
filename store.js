342a32078059d2bbd19c3871db66e16f36d33cac
Keitsun <ashirogimoto109@gmail.com>
2020-09-18 12:19:37 +0200

    7th commit

--- store.js
+++ store.js
@@ -5,13 +5,3 @@ function opennav(){
 function closenav(){
     document.getElementById("mobile-menu").style.width = "0"
 }
-
-
-
-
-
-document.querySelector('.img-btn').addEventListener('click', function()
-	{
-		document.querySelector('.container').classList.toggle('s-signup')
-	}
-);
